982141c1c376dbd1129a8b2aac74aae5886ab2b0
Haifei.Wang <haifei.wang@dianping.com>
2017-02-24 12:33:08 +0800

    add func

--- file.go
+++ file.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"io"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 )
 
 // FileExists return a bool value that file is exist or not
@@ -65,4 +69,43 @@ func FileMD5(file *os.File) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(md5Ctx.Sum(nil)), nil
+}
+
+// Mkdir create directory decursively
+func Mkdir(dir string, mode os.FileMode) error {
+	dir = strings.TrimSpace(dir)
+	if dir == "" || !path.IsAbs(dir) {
+		return errors.New("请正确设置目录的绝对路径！")
+	}
+	if !DirectoryExists(dir) {
+		e := Mkdir(path.Dir(dir), mode)
+		if e != nil {
+			return e
+		}
+		return os.Mkdir(dir, mode)
+	}
+	return nil
+}
+
+// WriteFile file directory decursively
+func WriteFile(filePath string, data []byte, cover bool, mode os.FileMode) error {
+	if data == nil {
+		data = make([]byte, 0, 0)
+	}
+	filePath = strings.TrimSpace(filePath)
+	if filePath == "" || !path.IsAbs(filePath) {
+		return errors.New("请正确设置文件的绝对路径！")
+	}
+	if !cover && FileExists(filePath) {
+		return errors.New("文件存在 [" + filePath + "]")
+	}
+	e := Mkdir(path.Dir(filePath), mode)
+	if e != nil {
+		return e
+	}
+	e = ioutil.WriteFile(filePath, data, mode)
+	if e != nil {
+		return e
+	}
+	return nil
 }
